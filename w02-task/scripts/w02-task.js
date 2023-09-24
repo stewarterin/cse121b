4cefffb5d8575570b736e8a1e27ec4b4e73231bb
stewarterin <erinlu@live.com>
2023-09-23 21:14:17 -0600

    w02

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,36 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = 'Erin Stewart';
+const currentYear = new Date().getFullYear();
+const profilePicture = 'images/erin23.JPG';
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = doucment.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector(img);
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+/* Step 5 - Array */
+let foodArray = ['Sushi', 'Salmon', 'Poppyseed muffins', 'Beef roast',
+    'Grilled cheese sandwich', 'Tomato soup', 'Carnitas tacos'];
+foodElement.textContent = foodArray;
+
+const foodItem = 'Pizza';
+foodArray.push(foodItem);
+
+foodElement.innerHTML += `<br>${favFoods}`;
+
+foodArray.shift();
+
+foodElement.innerHTML += `<br>${favFoods}`;
+
+
